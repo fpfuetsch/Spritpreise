dc9905b8dbc7f13985fe590d4e26ee1a2ea7bddd
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2019-07-30 14:34:55 +0200

    externals.

--- server/webpack.serverless.config.js
+++ server/webpack.serverless.config.js
@@ -5,10 +5,14 @@ module.exports = {
   entry: './src/lambda.js',
   target: 'node',
   output: {
-    path: path.join(__dirname, 'dist/serverless'),
-    filename: 'api.js'
+    path: path.resolve(__dirname, 'dist/serverless'),
+    filename: 'api.js',
+    libraryTarget: 'commonjs',
   },
   plugins: [
     new webpack.EnvironmentPlugin(['API_KEY', 'API_BASE_PATH', 'DB_URL'])
+  ],
+  externals: [
+    /^(?!\.|\/).+/i,
   ],
 }
