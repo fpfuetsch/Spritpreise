75dcdd5d2e3c0975857b7ed9b29ac5e5d994d4ca
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2019-07-30 14:01:02 +0200

    new webpack config

--- server/webpack.serverless.config.js
+++ server/webpack.serverless.config.js
@@ -1,70 +1,25 @@
-const path = require("path")
-const fs = require("fs")
-const webpack = require('webpack')
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
 
-// -- Webpack configuration --
+var nodeModules = {};
+fs.readdirSync('../node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
 
-const config = {}
-
-// Application entry point
-config.entry = "./src/lambda.js"
-
-// We build for node
-config.target = "node"
-
-// Node module dependencies should not be bundled
-config.externals = fs.readdirSync("../node_modules")
-  .reduce(function(acc, mod) {
-    if (mod === ".bin") {
-      return acc
-    }
-
-    acc[mod] = "commonjs " + mod
-    return acc
-  }, {})
-
-// We are outputting a real node app!
-config.node = {
-  console: false,
-  global: false,
-  process: false,
-  Buffer: false,
-  __filename: false,
-  __dirname: false,
-}
-
-// Output files in the build/ folder
-config.output = {
-  path: path.join(__dirname, "dist/serverless"),
-  filename: "api.js",
-}
-
-config.resolve = {
-  extensions: [
-    ".js",
-    ".json",
+module.exports = {
+  entry: './src/lambda.js',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'api.js'
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin(['API_KEY', 'API_BASE_PATH', 'DB_URL'])
   ],
+  externals: nodeModules
 }
-
-config.module = {}
-
-config.module.rules = [
-
-  // Use babel and eslint to build and validate JavaScript
-  {
-    test: /\.js$/,
-    exclude: /node_modules/,
-  },
-
-  // Allow loading of JSON files
-  {
-    test: /\.json$/,
-    loader: "json",
-  },
-]
-
-config.plugins = [
-  new webpack.EnvironmentPlugin(['API_KEY', 'API_BASE_PATH', 'DB_URL'])
-]
-
-module.exports = config
