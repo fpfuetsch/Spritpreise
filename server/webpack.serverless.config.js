e4854bdc465889f0f8a588aca89e918beba6cda8
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2019-07-30 14:06:47 +0200

    include node modules.

--- server/webpack.serverless.config.js
+++ server/webpack.serverless.config.js
@@ -1,15 +1,5 @@
 var webpack = require('webpack');
 var path = require('path');
-var fs = require('fs');
-
-var nodeModules = {};
-fs.readdirSync('../node_modules')
-  .filter(function(x) {
-    return ['.bin'].indexOf(x) === -1;
-  })
-  .forEach(function(mod) {
-    nodeModules[mod] = 'commonjs ' + mod;
-  });
 
 module.exports = {
   entry: './src/lambda.js',
@@ -21,5 +11,4 @@ module.exports = {
   plugins: [
     new webpack.EnvironmentPlugin(['API_KEY', 'API_BASE_PATH', 'DB_URL'])
   ],
-  externals: nodeModules
 }
